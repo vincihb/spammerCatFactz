6d5e16df50074a1b0bf4ac674916769bbdfaf4fd
vincihb <user@Users-MacBook-Pro.local>
2018-09-09 20:29:38 -0400

    cat factz

--- catFacts.py
+++ catFacts.py
@@ -1,3 +1,15 @@
 import smtplib
+from email.mime.text import MIMEText
 
+basicEmail = open(basicEmail.txt, 'rb')
+msg = MIMEText(basicEmail.read())
+basicEmail.close()
+
+print(msg);
+
+#catFactz = open(catFactsText.txt
+
+#msg['Subject'] = 'The contents of %s'
+#msg['From'] = me
+#msg['To'] = you
 
